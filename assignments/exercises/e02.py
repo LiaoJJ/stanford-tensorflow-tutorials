b550edddcbc881b0fcc7cf4dbf6d26681e39442a
Your Name <jjliao14@gmail.com>
2017-09-16 19:30:31 +0800

    e02.py finish the work of heart disease assignment of cs20si, it's finished with a accuracy of 0.73. I don't know whether there is wrong in my program, debug a afternoon ,it's very hard and a cost of time.so i don't want to make the program more perfect.

--- assignments/exercises/e02.py
+++ assignments/exercises/e02.py
@@ -10,9 +10,9 @@ TRAIN_SET_DIM  = 9
 TEST_SET_NUM   = 62
 
 BATCH_SIZE = 100
-HIDDEN_SIZE= 100
+HIDDEN_SIZE= 4
 LOGIT_SIZE = 2
-LEARNING_RATE = 0.01
+LEARNING_RATE = 0.1
 class lrmodel:
     def __init__(self,file_path,batch_size,train_set_dim,hidden_size,logit_size,lr,train_set_num,test_set_num):
         self.file_path=file_path
@@ -57,25 +57,26 @@ class lrmodel:
         self.X = tf.placeholder(tf.float32,shape=[self.batch_size,self.train_set_dim])
         self.y = tf.placeholder(tf.float32,shape=[self.batch_size,self.logit_size])
 
-        self.W1 = tf.Variable(tf.truncated_normal(shape=[self.train_set_dim,self.hidden_size],stddev=0.01))
+        self.W1 = tf.Variable(tf.truncated_normal(shape=[self.train_set_dim,self.hidden_size],stddev=0.1))
         self.b1 = tf.Variable(tf.zeros(shape=[self.hidden_size]))
 
-        self.W2 = tf.Variable(tf.truncated_normal(shape=[self.hidden_size,self.logit_size],stddev=0.01))
+        self.W2 = tf.Variable(tf.truncated_normal(shape=[self.hidden_size,self.logit_size],stddev=0.1))
         self.b2 = tf.Variable(tf.zeros(shape=self.logit_size))
 
     def _create_optimizer(self):
         hidden = tf.matmul(self.X,self.W1)+self.b1
         logit = tf.matmul(hidden,self.W2)+self.b2
+        # logit = tf.nn.relu(logi)
         entropy = tf.nn.softmax_cross_entropy_with_logits(labels=self.y,logits=logit)
         self.loss = tf.reduce_mean(entropy)
         # for train
-        self.optimizer = tf.train.GradientDescentOptimizer(self.lr).minimize(self.loss)
-        # for test
+        self.optimizer = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
+        # for train_acc
         preds = tf.nn.softmax(logits=logit)
-        correct_preds = tf.equal(tf.arg_max(preds,1),tf.arg_max(self.y,1))
-        self.accuracy = tf.reduce_sum(tf.cast(correct_preds[0:self.test_set_num],tf.float32))
+        correct_preds = tf.equal(tf.argmax(preds,1),tf.argmax(self.y,1))
+        self.accuracy = tf.reduce_sum(tf.cast(correct_preds,tf.float32))
         # for debug
-        self.debug = self.y
+        self.debug = correct_preds
 
 
     def build_graph(self):
@@ -83,29 +84,33 @@ class lrmodel:
         self._create_variable()
         self._create_optimizer()
 
-    def train_model(self,epoch,print_steps=4, test=0):
+    def train_model(self,epoch,print_steps=4, test=1):
         with tf.Session() as sess:
+            writer = tf.summary.FileWriter('./graphs/logistic_reg', sess.graph)
             sess.run(tf.global_variables_initializer())
             for e in range(epoch):
                 total_loss = 0
-                for step in range(int(self.train_set_num/self.batch_size)):
+                total_train_acc=0
+                for step in range(4):
                     xxx = self.x_train[step * self.batch_size: (step + 1) * self.batch_size,:]
                     yyy = self.y_train[step * self.batch_size: (step + 1) * self.batch_size,:]
                     feed_dict = {self.X:xxx, self.y:yyy}
 
-                    loss,_,debug=sess.run([self.loss,self.optimizer,self.debug],feed_dict=feed_dict)
+                    [loss,_,debug,train_acc]=sess.run([self.loss,self.optimizer,self.debug,self.accuracy],feed_dict=feed_dict)
                     # debug
                     # print(debug)
                     total_loss+=loss
-                    if((step+1)/print_steps):
-                        print("epoch:{} step:{} loss:{:f}".format(e,step,total_loss/4))
-                        pass
-                if(test==1):
-                    xxx = self.x_test[0:-1, :]
-                    yyy = self.y_test[0:-1, :]
-                    feed_dict = {self.X: xxx, self.y: yyy}
-                    [acc] = sess.run([self.accuracy], feed_dict=feed_dict)
-                    print("test accuracy: {}".format(acc/self.test_set_num))
+                    total_train_acc+=train_acc
+                if(1):
+                    print("epoch:{} loss:{} train_acc:{}".format(e,total_loss,total_train_acc/400))
+                    pass
+                # if(test==1):
+                #     xxx = self.x_test[0:-1, :]
+                #     yyy = self.y_test[0:-1, :]
+                #     feed_dict = {self.X: xxx, self.y: yyy}
+                #     [acc] = sess.run([self.accuracy], feed_dict=feed_dict)
+                #     # print("test accuracy: {.3f}".format(acc/self.test_set_num))
+            writer.close()
 
 
 
@@ -122,7 +127,7 @@ class lrmodel:
 def main():
     model = lrmodel(FILE_PATH,BATCH_SIZE,TRAIN_SET_DIM,HIDDEN_SIZE,LOGIT_SIZE,LEARNING_RATE,TRAIN_SET_NUM,TEST_SET_NUM)
     model.build_graph()
-    model.train_model(epoch=50,test=1)
+    model.train_model(epoch=100,test=1)
     # model.test_model()
 
 if __name__ == '__main__':
